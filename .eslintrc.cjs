aea57264349639713e019d0b798844ab75e7f20a
SHO <s.sue@hitsuzen.co.jp>
2025-08-22 12:26:04 +0900

    chore: ESLint/Prettier/EditorConfig を導入しスタイルを統一

---
+++ .eslintrc.cjs
@@ -0,0 +1,25 @@
+/* eslint-env node */
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true, node: true },
+  parser: '@typescript-eslint/parser',
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: 'detect' } },
+  plugins: ['@typescript-eslint', 'react', 'react-hooks'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      { argsIgnorePattern: '^_', varsIgnorePattern: '^_', ignoreRestSiblings: true },
+    ],
+  },
+}
+
